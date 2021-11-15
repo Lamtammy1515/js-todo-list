3c593673f66085bc35465ec08d3c8532a4724011
tammylam <lamtammy1515@gmail.com>
2021-11-15 15:29:51 -0600

    added some css styling to check button

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ todoList.addEventListener('click', deleteCheck);
 
 function addTodo(e){
     e.preventDefault()
-    console.log('hello')
     //todo div
     const todoDiv = document.createElement('div');
     todoDiv.classList.add("todo")
@@ -44,5 +43,10 @@ function deleteCheck(e) {
  if (item.classList[0] === 'trash-btn'){
      const todo = item.parentElement;
      todo.remove();
+ }
+
+ if(item.classList[0] === "complete-btn"){
+     const todo = item.parentElement
+     todo.classList.toggle('completed');
  }
 }
